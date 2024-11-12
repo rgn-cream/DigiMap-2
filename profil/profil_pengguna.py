72cd47c7384012497ca7971f770ab75240caf7ab
Muzaaja <muh.zaidan.rahman@upi.edu>
2024-11-12 20:19:10 +0700

    perubahan data

--- profil/profil_pengguna.py
+++ profil/profil_pengguna.py
@@ -1,13 +1,14 @@
 profil_pengguna = {
     "nama": "Nama Pengguna",
     "NIM": "123456789",
-    "kelas": "TI-1A",
-    "tanggal_lahir": "01-01-2000",
+    "kelas": "RPL-1B",
+    "tanggal_lahir": "01-01-2006",
     "no_telepon": "081234567890",
     "email": "email@domain.com"
 }
 
 def tampilkan_profil():
+    print("="*40)
     print("=== Profil Pengguna ===")
     print(f"Nama: {profil_pengguna['nama']}")
     print(f"NIM: {profil_pengguna['NIM']}")
@@ -15,6 +16,6 @@ def tampilkan_profil():
     print(f"Tanggal Lahir: {profil_pengguna['tanggal_lahir']}")
     print(f"No Telepon: {profil_pengguna['no_telepon']}")
     print(f"Email: {profil_pengguna['email']}")
-
-
+    print("="*40)
+    
 tampilkan_profil()
