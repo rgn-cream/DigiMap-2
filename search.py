0e521b897887723e384d7ddf51b241ba7865e2dc
Riikq <tariqahmad0510@gmail.com>
2024-11-13 13:59:53 +0700

    menambahkan search.py(updated)
    melengkapi data kelas tiap prodi per harinya

---
+++ search.py
@@ -0,0 +1,117 @@
+jadwal_kuliah = {
+    "Senin": [
+        {"waktu_mulai": "07:00", "waktu_selesai": "10:20", "kelas": "PGSD 1A", "kode_mk": "GD567", "nama_mk": "Dasar Bahasa Indonesia", "sks": 4, "dosen": ["Dr. Yunus Abidin, M.Pd.", "Fully Rakhmayanti, M.Pd."], "ruang": "20.4B.05.002"},
+        {"waktu_mulai": "10:20", "waktu_selesai": "12:00", "kelas": "PGSD 1A", "kode_mk": "KUI10", "nama_mk": "Pendidikan Pancasila", "sks": 2, "dosen": ["Dr. Dinie Anggraini Dewi, M.Pd."], "ruang": "20.4B.04.001"},
+        {"waktu_mulai": "07:00", "waktu_selesai": "10:20", "kelas": "PGSD 1B", "kode_mk": "GD569", "nama_mk": "Dasar Matematika", "sks": 4, "dosen": ["Dr. Tita Mulyati, M.Pd."], "ruang": "20.4B.05.005"},
+        {"waktu_mulai": "13:00", "waktu_selesai": "14:40", "kelas": "PGSD 1B", "kode_mk": "KU110", "nama_mk": "Pendidikan Pancasila", "sks": 2, "dosen": ["Dr. Dinie Anggraini Dewi, M.Pd."], "ruang": "20.4E.03.003"},
+        {"waktu_mulai": "07:00", "waktu_selesai": "10:20", "kelas": "PGSD 1C", "kode_mk": "GD573", "nama_mk": "Esensial IPS", "sks": 4, "dosen": ["Dra. Tin Rustini, M.Pd."], "ruang": "20.4B.03.006"},
+        {"waktu_mulai": "10:20", "waktu_selesai": "12:00", "kelas": "PGSD 1C", "kode_mk": "DK300", "nama_mk": "Landasan Pendidikan", "sks": 2, "dosen": ["Dr. Yusuf Tri Herlambang, M.Pd."], "ruang": "20.4B.05.007"},
+        {"waktu_mulai": "07:00", "waktu_selesai": "10:20", "kelas": "PGSD 1D", "kode_mk": "GD567", "nama_mk": "Dasar Bahasa Indonesia", "sks": 4, "dosen": ["Dr. Kurniawati, M.Pd.", "S. Nailul Muna Aljamaliah, M.Pd."], "ruang": "20.4B.05.001"},
+        {"waktu_mulai": "07:00", "waktu_selesai": "10:20", "kelas": "PGSD 1E", "kode_mk": "GD569", "nama_mk": "Dasar Matematika", "sks": 4, "dosen": ["Dr. Yeni Yuniarti, M.Pd."], "ruang": "20.4B.03.007"},
+        {"waktu_mulai": "14:40", "waktu_selesai": "16:20", "kelas": "PGSD 1E", "kode_mk": "KU110", "nama_mk": "Pendidikan Pancasila", "sks": 2, "dosen": ["Yayang Furi Furnamasari, M.Pd."], "ruang": "20.4E.03.003"},
+        {"waktu_mulai": "07:00", "waktu_selesai": "10:20", "kelas": "S2-PGSD 1A", "kode_mk": "GD573", "nama_mk": "Konsep Dasar IPS", "sks": 3, "dosen": ["Dr. Tri Rustini, M.Pd."], "ruang": "20.4B.05.006"},
+        {"waktu_mulai": "07:00", "waktu_selesai": "10:20", "kelas": "S2-PGSD 1A", "kode_mk": "GD567", "nama_mk": "Konsep Dasar Bahasa Indonesia", "sks": 3, "dosen": ["Dr. Yunus Abidin, M.Pd."], "ruang": "20.4B.05.002"},
+        {"waktu_mulai": "07:00", "waktu_selesai": "10:20", "kelas": "TEKKOM 1A", "kode_mk": "TR102", "nama_mk": "Matematika Diskrit", "sks": 4, "dosen": ["Devi Apriyanti Rimadhani Agustini, S.Si., M.Si."], "ruang": "20.4B.05.001"},
+        {"waktu_mulai": "13:00", "waktu_selesai": "16:20", "kelas": "TEKKOM 1A", "kode_mk": "TR200", "nama_mk": "Pemrograman Dasar", "sks": 4, "dosen": ["Dr. Eng. Munawir, S.Kom., M.T."], "ruang": "20.4E.02.001"},
+        {"waktu_mulai": "07:00", "waktu_selesai": "10:20", "kelas": "TEKKOM 1B", "kode_mk": "TR200", "nama_mk": "Pemrograman Dasar", "sks": 4, "dosen": ["Dr. Eng. Munawir, S.Kom., M.T."], "ruang": "20.4E.02.001"},
+        {"waktu_mulai": "13:00", "waktu_selesai": "15:30", "kelas": "TEKKOM 1B", "kode_mk": "TR101", "nama_mk": "Fisika", "sks": 3, "dosen": ["Dr. Dede Trie Kurniawan, S.Si., M.Pd."], "ruang": "20.4E.02.004"},
+        {"waktu_mulai": "07:00", "waktu_selesai": "09:30", "kelas": "TEKKOM 1C", "kode_mk": "TR101", "nama_mk": "Fisika", "sks": 3, "dosen": ["Dr. Dede Trie Kurniawan, S.Si., M.Pd."], "ruang": "20.4E.02.004"},
+        {"waktu_mulai": "10:20", "waktu_selesai": "12:00", "kelas": "TEKKOM 1C", "kode_mk": "KU106", "nama_mk": "Pendidikan Bahasa Indonesia", "sks": 2, "dosen": ["Fully Rakhmayanti, M.Pd."], "ruang": "20.4B.05.005"},
+        {"waktu_mulai": "13:20", "waktu_selesai": "16:20", "kelas": "TEKKOM 1C", "kode_mk": "TR102", "nama_mk": "Matematika Diskrit", "sks": 4, "dosen": ["Devi Apriyanti Rimadhani Agustini, S.Si., M.Si."], "ruang": "20.4B.05.001"},
+        {"waktu_mulai": "07:00", "waktu_selesai": "12:00", "kelas": "PGPAUD 1A", "kode_mk": "UD110", "nama_mk": "Psikologi Perkembangan Anak", "sks": 4, "dosen": ["Ririn Despriliani, S.Pd., M.Si."], "ruang": "20.4B.05.000"},
+        {"waktu_mulai": "10:20", "waktu_selesai": "12:00", "kelas": "PGPAUD 1A", "kode_mk": "KU101", "nama_mk": "Pendidikan Agama Kristen", "sks": 2, "dosen": ["Suka Prayanta Pendia, S.Pd., M.P.Fis."], "ruang": "20.4B.03.007"},
+        {"waktu_mulai": "14:40", "waktu_selesai": "16:20", "kelas": "PGPAUD 1A", "kode_mk": "KU100", "nama_mk": "Pendidikan Agama Islam", "sks": 2, "dosen": ["Dr. Arif Hidayat, M.Pd.I."], "ruang": "20.4E.03.002"},
+        {"waktu_mulai": "10:20", "waktu_selesai": "12:00", "kelas": "PGPAUD 1B", "kode_mk": "KU100", "nama_mk": "Pendidikan Agama Islam", "sks": 2, "dosen": ["Dr. Arif Hidayat, M.Pd.I."], "ruang": "20.4E.03.002"},
+        {"waktu_mulai": "13:00", "waktu_selesai": "16:20", "kelas": "PGPAUD 1B", "kode_mk": "UD110", "nama_mk": "Psikologi Perkembangan Anak", "sks": 4, "dosen": ["Ririn Despriliani, S.Pd., M.Si."], "ruang": "20.4B.05.000"},
+        {"waktu_mulai": "10:20", "waktu_selesai": "12:00", "kelas": "PGPAUD 1C", "kode_mk": "KU110", "nama_mk": "Pendidikan Pancasila", "sks": 2, "dosen": ["Yayang Furi Furnamasari, M.Pd."], "ruang": "20.4B.05.001"},
+        {"waktu_mulai": "13:00", "waktu_selesai": "14:40", "kelas": "PGPAUD 1C", "kode_mk": "KU100", "nama_mk": "Pendidikan Agama Islam", "sks": 2, "dosen": [""], "ruang": "20.4B.05.001"},
+        {"waktu_mulai": "07:00", "waktu_selesai": "10:20", "kelas": "RPL 1A", "kode_mk": "RL118", "nama_mk": "Matematika Dasar", "sks": 4, "dosen": ["Raditya Muhammad, M.T."], "ruang": "20.4B.02.006"},
+        {"waktu_mulai": "13:00", "waktu_selesai": "15:30", "kelas": "RPL 1A", "kode_mk": "RL116", "nama_mk": "Pengantar Rekayasa Perangkat Lunak", "sks": 3, "dosen": ["Dian Anggraini, S.ST., M.T."], "ruang": "20.4E.02.006"},
+        {"waktu_mulai": "09:30", "waktu_selesai": "12:00", "kelas": "RPL 1B", "kode_mk": "RL116", "nama_mk": "Pengantar Rekayasa Perangkat Lunak", "sks": 3, "dosen": ["Dian Anggraini, S.ST., M.T."], "ruang": "20.4E.02.006"},
+        {"waktu_mulai": "07:00", "waktu_selesai": "09:30", "kelas": "RPL 1C", "kode_mk": "RL116", "nama_mk": "Pengantar Rekayasa Perangkat Lunak", "sks": 3, "dosen": ["Dian Anggraini, S.ST., M.T."], "ruang": "20.4E.02.006"},
+        {"waktu_mulai": "07:00", "waktu_selesai": "10:20", "kelas": "PMM 1A", "kode_mk": "KU100", "nama_mk": "Ilustrasi", "sks": 4, "dosen": ["Dian Rinjani, S.Pd., M.Pd."], "ruang": "20.4B.04.001"},
+        {"waktu_mulai": "10:20", "waktu_selesai": "12:00", "kelas": "PMM 1A", "kode_mk": "KU100", "nama_mk": "Pendidikan Agama Islam", "sks": 2, "dosen": ["Asep Rudi Nurjaman, M.Pd."], "ruang": "20.4B.04.006"},
+        {"waktu_mulai": "13:00", "waktu_selesai": "15:30", "kelas": "PMM 1A", "kode_mk": "KU100", "nama_mk": "Matematika Diskrit", "sks": 3, "dosen": ["Nurhidayatulloh, S.Pd., M.T."], "ruang": "20.4B.04.001"},
+        {"waktu_mulai": "13:00", "waktu_selesai": "16:20", "kelas": "PMM 1B", "kode_mk": "KU100", "nama_mk": "Ilustrasi", "sks": 4, "dosen": ["Dian Rinjani, S.Pd., M.Pd."], "ruang": "20.4B.04.001"},
+        {"waktu_mulai": "08:40", "waktu_selesai": "10:20", "kelas": "PMM 1C", "kode_mk": "KU100", "nama_mk": "Pendidikan Agama Islam", "sks": 2, "dosen": ["Asep Rudi Nurjaman, M.Pd"], "ruang": "20.4B.02.007"},
+        {"waktu_mulai": "13:00", "waktu_selesai": "14:40", "kelas": "PMM 1C", "kode_mk": "KU100", "nama_mk": "Pendidikan Pancasila", "sks": 2, "dosen": ["Yayang Furi Furnamasari, M.Pd."], "ruang": "20.4B.02.007"}
+
+
+
+
+    ],
+    "Selasa": [
+        {"waktu_mulai": "07:00", "waktu_selesai": "10:20", "kelas": "PGSD 1A", "kode_mk": "GD569", "nama_mk": "Dasar Matematika", "sks": 4, "dosen": ["Dr. Tita Mulyati, M.Pd."], "ruang": "20.4B.05.002"},
+        {"waktu_mulai": "10:20", "waktu_selesai": "12:00", "kelas": "PGSD 1A", "kode_mk": "DK300", "nama_mk": "Landasan Pendidikan", "sks": 2, "dosen": ["Dr. Yusuf Tri Herlambang, M.Pd."], "ruang": "20.4B.05.006"},
+        {"waktu_mulai": "07:00", "waktu_selesai": "10:20", "kelas": "PGSD 1B", "kode_mk": "GD567", "nama_mk": "Dasar Bahasa Indonesia", "sks": 4, "dosen": ["Dr. Kurniawati, M.Pd.", "Fully Rakhmayanti, M.Pd."], "ruang": "20.4B.05.005"},
+        {"waktu_mulai": "07:00", "waktu_selesai": "10:20", "kelas": "PGSD 1C", "kode_mk": "GD573", "nama_mk": "Esensial IPS", "sks": 4, "dosen": ["Dra. Tin Rustini, M.Pd."], "ruang": "20.4B.03.006"},
+        {"waktu_mulai": "13:00", "waktu_selesai": "15:30", "kelas": "PGSD 1C", "kode_mk": "CB401", "nama_mk": "Design Thinking", "sks": 3, "dosen": ["Dr. Rendi Restiana Sukardi, M.Pd."], "ruang": "20.4B.05.002"},
+        {"waktu_mulai": "07:00", "waktu_selesai": "10:20", "kelas": "PGSD 1D", "kode_mk": "GD573", "nama_mk": "Esensial IPS", "sks": 4, "dosen": ["Dra. Tin Rustini, M.Pd."], "ruang": "20.4B.03.006"},
+        {"waktu_mulai": "07:00", "waktu_selesai": "10:20", "kelas": "PGSD 1E", "kode_mk": "GD573", "nama_mk": "Esensial IPS", "sks": 4, "dosen": ["Dr. Muh. Husen Arifin, S.E., M.Pd."], "ruang": "20.4B.05.006"},
+        {"waktu_mulai": "07:00", "waktu_selesai": "10:20", "kelas": "S2-PGSD 1A", "kode_mk": "GD569", "nama_mk": "Konsep Dasar Matematika", "sks": 3, "dosen": ["Dr. Tita Mulyati, M.Pd."], "ruang": "20.4B.05.002"},
+        {"waktu_mulai": "07:00", "waktu_selesai": "09:30", "kelas": "TEKKOM 1A", "kode_mk": "TR101", "nama_mk": "Fisika", "sks": 3, "dosen": ["Dr. Dede Trie Kurniawan, S.Si., M.Pd."], "ruang": "20.4E.01.004"},
+        {"waktu_mulai": "09:30", "waktu_selesai": "12:00", "kelas": "TEKKOM 1A", "kode_mk": "PT400", "nama_mk": "Keterampilan Bahasa Inggris", "sks": 3, "dosen": ["Winti Ananthia, S.Pd., M.Pd."], "ruang": "20.4B.05.009"},
+        {"waktu_mulai": "07:00", "waktu_selesai": "10:20", "kelas": "TEKKOM 1B", "kode_mk": "TR102", "nama_mk": "Matematika Diskrit", "sks": 4, "dosen": ["Devi Apriyanti Rimadhani Agustini, S.Si., M.Si."], "ruang": "20.4E.01.004"},
+        {"waktu_mulai": "10:20", "waktu_selesai": "12:00", "kelas": "TEKKOM 1B", "kode_mk": "KU106", "nama_mk": "Pendidikan Bahasa Indonesia", "sks": 2, "dosen": ["Fully Rakhmayanti, M.Pd."], "ruang": "20.4B.05.005"},
+        {"waktu_mulai": "07:00", "waktu_selesai": "10:20", "kelas": "TEKKOM 1C", "kode_mk": "TR200", "nama_mk": "Pemrograman Dasar", "sks": 4, "dosen": ["Dr. Eng. Munawir, S.Kom., M.T."], "ruang": "20.4E.02.001"} #last edited
+        
+
+    ],
+    "Rabu": [
+        {"waktu_mulai": "07:00", "waktu_selesai": "10:20", "kelas": "PGSD 1A", "kode_mk": "GD573", "nama_mk": "Esensial IPS", "sks": 4, "dosen": ["Dra. Tin Rustini, M.Pd."], "ruang": "20.4B.03.006"},
+        {"waktu_mulai": "13:00", "waktu_selesai": "14:40", "kelas": "PGSD 1A", "kode_mk": "DK301", "nama_mk": "Psikologi Pendidikan", "sks": 2, "dosen": ["Triana Lestari, S.Psi, M.Pd."], "ruang": "20.4B.02.002"},
+        {"waktu_mulai": "07:00", "waktu_selesai": "08:40", "kelas": "PGSD 1B", "kode_mk": "DK301", "nama_mk": "Psikologi Pendidikan", "sks": 2, "dosen": ["Triana Lestari, S.Psi, M.Pd."], "ruang": "20.4B.02.002"},
+        {"waktu_mulai": "13:00", "waktu_selesai": "14:40", "kelas": "PGSD 1B", "kode_mk": "DK300", "nama_mk": "Landasan Pendidikan", "sks": 2, "dosen": ["Dr. Yusuf Tri Herlambang, M.Pd."], "ruang": "20.4B.05.007"},
+        {"waktu_mulai": "07:00", "waktu_selesai": "10:20", "kelas": "PGSD 1C", "kode_mk": "GD567", "nama_mk": "Dasar Bahasa Indonesia", "sks": 4, "dosen": ["Dr. Kurniawati, M.Pd.", "Fully Rakhmayanti, M.Pd."], "ruang": "20.4B.03.007"},
+        {"waktu_mulai": "07:00", "waktu_selesai": "10:20", "kelas": "PGSD 1D", "kode_mk": "GD569", "nama_mk": "Dasar Matematika", "sks": 4, "dosen": ["Dr. Tita Mulyati, M.Pd."], "ruang": "20.4B.05.005"}
+    ],
+    "Kamis": [
+        {"waktu_mulai": "07:00", "waktu_selesai": "08:40", "kelas": "PGSD 1A", "kode_mk": "DK301", "nama_mk": "Psikologi Pendidikan", "sks": 2, "dosen": ["Triana Lestari, S.Psi, M.Pd."], "ruang": "20.4B.05.007"},
+        {"waktu_mulai": "13:00", "waktu_selesai": "14:40", "kelas": "PGSD 1A", "kode_mk": "DK302", "nama_mk": "Dasar IPS", "sks": 2, "dosen": ["Dr. Yudianto, S.Pd, M.Pd."], "ruang": "20.4B.03.005"},
+        {"waktu_mulai": "07:00", "waktu_selesai": "10:20", "kelas": "PGSD 1B", "kode_mk": "GD573", "nama_mk": "Esensial IPS", "sks": 4, "dosen": ["Dra. Tin Rustini, M.Pd."], "ruang": "20.4B.05.002"}
+    ],
+    "Jumat": [
+        {"waktu_mulai": "07:00", "waktu_selesai": "10.20", "kelas": "PGSD 1F", "kode_mk": "GD567", "nama_mk": "Dasar Bahasa Indonesia", "sks": 4, "dosen": ["Dr. Kurniawati, M.Pd.", "S. Nailul Muna Aljamaliah, M.Pd"], "ruang": "20.4B.05.002"}
+    ]
+}
+
+print("\n==========Pencarian Jadwal Kelas==========")
+
+def cari_jadwal(jadwal):
+    hari = input("\nMasukkan hari: ").capitalize()
+    kelas = input("\nMasukkan jurusan dan kelas: ").upper()
+
+    hasil = []
+    for hari_kuliah, jadwal_harian in jadwal.items():
+        if hari and hari_kuliah != hari:
+            continue
+        for mata_kuliah in jadwal_harian:
+            if kelas and mata_kuliah["kelas"].upper() != kelas:
+                continue
+            hasil.append({
+                "hari": hari_kuliah,
+                "waktu_mulai": mata_kuliah["waktu_mulai"],
+                "waktu_selesai": mata_kuliah["waktu_selesai"],
+                "kelas": mata_kuliah["kelas"],
+                "kode_mk": mata_kuliah["kode_mk"],
+                "nama_mk": mata_kuliah["nama_mk"],
+                "sks": mata_kuliah["sks"],
+                "dosen": mata_kuliah["dosen"],
+                "ruang": mata_kuliah["ruang"]
+            })
+
+ # Tampilkan hasil
+    print("")
+    if hasil:
+        # Print header
+        print(f"{'Hari':<12}{'Waktu Mulai':<15}{'Waktu Selesai':<15}{'Kelas':<12}{'Kode MK':<10}{'Nama MK':<25}{'SKS':<5}{'Dosen':<40}{'Ruang':<15}")
+        print("=" * 150)  # Garis pemisah
+        for jadwal_item in hasil:
+            dosen = jadwal_item['dosen'][0] if isinstance(jadwal_item['dosen'], list) else jadwal_item['dosen']
+            
+            print(f"\n{str(jadwal_item['hari']):<12}{str(jadwal_item['waktu_mulai']):<15}{str(jadwal_item['waktu_selesai']):<15}{str(jadwal_item['kelas']):<12}{str(jadwal_item['kode_mk']):<10}{str(jadwal_item['nama_mk']):<25}{str(jadwal_item['sks']):<5}{str(dosen):<40}{str(jadwal_item['ruang']):<15}\n")
+    else:   
+        print("\nTidak ada jadwal yang sesuai dengan kriteria pencarian\n")
+
+# Penggunaan
+cari_jadwal(jadwal_kuliah)
